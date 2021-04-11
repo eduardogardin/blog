222bf64ffea0b10457b92b9248b88c517aa70975
Eduardo Gardin <eduardoj.gardin@gmail.com>
2021-04-11 20:43:37 -0300

    Testing GA tag

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'G-SSV4YXKNXG',
   useKatex: false,
   menu: [
     {
